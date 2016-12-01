efa1546585994f202ec660e38863ef9cd522031a
qinzhihao <scottzhq@163.com>
2016-12-01 10:37:36 +0800

    when created a group, join it automatically

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -12,7 +12,7 @@ class GroupsController < ApplicationController
 
   def show
     @group = Group.find(params[:id])
-    @posts = @group.posts.recent.paginate(:page => params[:page], :per_page => 5)  
+    @posts = @group.posts.recent.paginate(:page => params[:page], :per_page => 5)
   end
 
   def edit
@@ -24,6 +24,7 @@ class GroupsController < ApplicationController
     @group.user = current_user
 
     if @group.save
+      current_user.join!(@group)
       redirect_to groups_path
     else
       render :new
@@ -46,6 +47,28 @@ class GroupsController < ApplicationController
     @group.destroy
     flash[:alert] = "Group deleted"
     redirect_to groups_path
+  end
+
+  def join
+    @group = Group.find(params[:id])
+    if !current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:notice] = "加入本讨论成功"
+    else
+      flash[:warning] = "你已经是本讨论版成员了"
+    end
+    redirect_to group_path(@group)
+  end
+
+  def quit
+    @group = Group.find(params[:id])
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:alert] = "你已退出讨论"
+    else
+      flash[:warning] = "你不是本讨论版成员，怎么退出 XD"
+    end
+    redirect_to group_path(@group)
   end
 
   private
